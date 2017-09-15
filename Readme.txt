9e59db7c4441f659edbcd686e64b128206762b58
JaganBaliah <jagan.mit@gmail.com>
2017-09-14 22:40:04 -0500

    Initial check-in

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+Goto spring.io and head to Spring initializr
+
+Group 		: io.jagan.springbootquickstart
+Artifact 	: course-si-api
+Description : Course SI API
+Selected Dependencies : Web
+
+Generate Project to get the zip file and then extract the zip file
+
+Import -> Existing Maven Projects -> Provide the above extracted directory
+
---
+++ src/main/java/io/jagan/springbootquickstart/CourseSiApiApplication.java
@@ -0,0 +1,12 @@
+package io.jagan.springbootquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseSiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseSiApiApplication.class, args);
+	}
+}
